062eac4d6c08f7ebdd610c7aee1ec64b708eaca3
VINOOTHNA0110 <60919284+VINOOTHNA0110@users.noreply.github.com>
2020-06-22 12:28:24 +0530

    Add files via upload

---
+++ LCD_PROTEUS.ino
@@ -0,0 +1,17 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(7,6,5,4,3,2); // sets the interfacing pins
+
+void setup() {
+// put your setup code here, to run once:
+lcd.begin(16, 2);
+}
+
+void loop() {
+lcd.setCursor(0,0);
+lcd.print("asss");// Displays HELLO WORLD on LCD
+delay(2000);
+lcd.setCursor(0,1);
+lcd.print("assdde");//Displays the string from second line.
+delay(2000);
+}
